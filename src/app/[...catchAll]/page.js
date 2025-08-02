143fede9470b16d04657c6af13af2b9302e1fadd
unknown <aananmunna420@gmail.com>
2025-08-02 15:29:59 +0600

    added a loading spinner

--- src/app/[...catchAll]/page.js
+++ src/app/[...catchAll]/page.js
@@ -1,16 +1,20 @@
-// app/[...catchAll]/page.jsx
+// app/not-found.jsx
 "use client";
-import { useParams } from "next/navigation";
+import Link from "next/link";
 
-export default function CatchAll() {
-  const params = useParams();
-  
+export default function NotFound() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <h1 className="text-3xl font-bold text-red-500">Page Not Found</h1>
-      <p className="mt-2 text-gray-700">
-        You tried visiting: <code>{JSON.stringify(params.catchAll)}</code>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 text-center px-4">
+      <h1 className="text-6xl font-extrabold text-blue-600">404</h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Oops! The page you're looking for doesn’t exist.
       </p>
+      <Link
+        href="/"
+        className="mt-6 inline-block px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+      >
+        Go Back Home
+      </Link>
     </div>
   );
 }
